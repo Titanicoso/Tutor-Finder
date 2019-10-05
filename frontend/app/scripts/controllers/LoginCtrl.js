0fb39f3ca599e4051364ec4ca0165bdf48be4a96
Sebastian Favaron <sebastianfavaron@gmail.com>
2019-10-04 21:49:51 -0300

    Remove temporary user login and registration services

--- frontend/app/scripts/controllers/LoginCtrl.js
+++ frontend/app/scripts/controllers/LoginCtrl.js
@@ -3,30 +3,9 @@ define(['tutorFinder'], function(tutorFinder) {
 
 	tutorFinder.controller('LoginCtrl', LoginCtrl);
 	
-	LoginCtrl.$inject = ['$scope', '$location']; /* 'AuthenticationService', 'FlashService' ]; */
-	function LoginCtrl($scope, $location /* , AuthenticationService /* , FlashService */) {
-        $scope.msg = 'This is the login view';
-        /* 
+	LoginCtrl.$inject = ['$scope', '$location'];
+	function LoginCtrl($scope, $location ) {
         var vm = this;
-		vm.login = login;
         vm.msg = 'This is the login view';
- */
-		/* (function initController() {
-            // reset login status
-            AuthenticationService.ClearCredentials();
-        })(); */
-
-        /* function login() {
-            vm.dataLoading = true;
-            AuthenticationService.Login(vm.username, vm.password, function (response) {
-                if (response.success) {
-                    AuthenticationService.SetCredentials(vm.username, vm.password);
-                    $location.path('/');
-                } else {
-                    FlashService.Error(response.message);
-                    vm.dataLoading = false;
-                }
-            });
-        }; */
 	};
 });
--- frontend/app/scripts/controllers/RegisterCtrl.js
+++ frontend/app/scripts/controllers/RegisterCtrl.js
@@ -3,33 +3,9 @@ define(['tutorFinder'], function(tutorFinder) {
 
 	tutorFinder.controller('RegisterCtrl', RegisterCtrl);
 	
-	RegisterCtrl.$inject = ['$scope', '$location']; /* , 'UserService' /*  , 'FlashService' , '$rootScope'];*/
-	function RegisterCtrl($scope, $location /* , UserService /* , FlashService , $rootScope */) {
-        
-        $scope.msg = 'This is the register view';
-
-		$scope.hello = function($scope) {
-            $scope.msg = 'hello';
-        };
-        
-        /* $scope.myCookieVal = $cookies.get('cookie');
-		$scope.setCookie = function(val) {
-            $cookies.put('cookie', val);
-        }; */
-        
-        /* $scope.register = function($scope) {
-            $scope.msg = 'Dont press that!';
-            $scope.dataLoading = true;
-            UserService.create($scope.user)
-                .then(function (response) {
-                    if (response.success) {
-                        FlashService.success('Registration successful', true);
-                        $location.path('#!/login');
-                    } else {
-                        FlashService.Error(response.message);
-                        $scope.dataLoading = false;
-                    }
-                });
-        }; */
+	RegisterCtrl.$inject = ['$scope', '$location'];
+	function RegisterCtrl($scope, $location) {
+        var vm = this;
+        vm.msg = 'This is the login view';
 	};
 });
