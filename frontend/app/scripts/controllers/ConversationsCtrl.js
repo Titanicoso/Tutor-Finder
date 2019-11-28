e601bdc97bff797ad2bbf8e1d33125a07e4f7bb5
Rodrigo Manuel Navarro Lajous <rnavarro@itba.edu.ar>
2019-11-28 17:13:23 -0300

    conversations controler

--- frontend/app/scripts/controllers/ConversationsCtrl.js
+++ frontend/app/scripts/controllers/ConversationsCtrl.js
@@ -1,11 +1,25 @@
 'use strict';
-define(['tutorFinder'], function(tutorFinder) {
+define(['tutorFinder', 'services/conversationService'], function(tutorFinder) {
 
 	tutorFinder.controller('ConversationsCtrl', ConversationsCtrl);
 	
-	ConversationsCtrl.$inject = ['$scope', '$rootScope'];
-	function ConversationsCtrl($scope, $rootScope) {
-
+	ConversationsCtrl.$inject = ['$scope', '$rootScope', 'conversationService'];
+	function ConversationsCtrl($scope, $rootScope, conversationService) {
 		$rootScope.appendTitle('CONVERSATIONS');
+
+		var currentPage = 1;
+
+		$scope.getPage = function(number) {
+			conversationService.getConversations(number)
+			.then(function(conversations) {
+				$scope.conversations = conversations;
+				currentPage = number;
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+		};
+
+		$scope.getPage(currentPage);
 	};
 });
